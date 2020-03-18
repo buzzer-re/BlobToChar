95b15f6e3ac0f1a45ca01ff67fdfe686b6affd0d
anderson <anderson@machine>
2020-03-18 13:58:38 -0300

    ADD: Load bytes as hex string

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,24 @@ int main(int argc, char const *argv[])
 
     const std::string blobTarget = argv[1];
     
+    std::string varName = argv[2];
 
     if (fileManager.exists(blobTarget)) {
         std::string codeBuilder;
 
-        codeBuilder = "char* v1= {";
+        codeBuilder = "char " + varName + "[] = ";
         
         std::unique_ptr<ByteArray> fileByteArray(fileManager.toByteArray(blobTarget));
+
+        char* hexRep = new char[4];  // 0xFF
+        
+        for (auto it = fileByteArray->begin(); it != fileByteArray->end(); ++it) {
+            std::sprintf(hexRep, "0x%x",  *it);
+            codeBuilder += std::string(hexRep) + ",";
+        }
+
+        delete[] hexRep;
+        codeBuilder += "};";
 
     }
 
